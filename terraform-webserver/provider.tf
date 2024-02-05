0fe74f6ac61c7fa40981385e73afecee5724a9c0
Ricardo Fahham <rfahham@hotmail.com>
2024-02-05 19:52:42 -0300

    add terraform-webserver/

---
+++ terraform-webserver/provider.tf
@@ -0,0 +1,6 @@
+# Specify the GCP Provider
+provider "google" {
+  project = "devops-rfahham-413414"
+  region  = "southamerica-east1"
+  credentials = "devops-rfahham-413414-552a02af80d8.json"
+}
---
+++ terraform-webserver/terraform.tfvars
@@ -0,0 +1,6 @@
+project_id = "gcp-project-id"
+  labels     = {
+    "environment" = "dev"
+    "team"        = "devops"
+    "application" = "webserver"
+  }
